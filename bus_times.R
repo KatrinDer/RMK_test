1eeb7a6fdf431468772d0192be4dff39a61a3fa9
KatrinDer <katrin.lehe@gmail.com>
2025-05-31 19:43:16 +0300

    Update bus_times.R

--- bus_times.R
+++ bus_times.R
@@ -3,3 +3,7 @@ Zoo <- c("08:05", "08:16", "08:28", "08:38", "08:48", "08:59") # departure from 
 Toompark <- c("08:18", "08:30", "08:41", "08:51", "09:01", "09:21") # arrival at Toompargi stop
 bus_times <- data.frame(Zoo,Toompark)
 head(bus_times)
+
+# changing the times suitable for calculation
+Zoo_posix <- as.POSIXct(Zoo, format="%H:%M")
+Toompark_posix <- as.POSIXct(Toompark, format="%H:%M")
