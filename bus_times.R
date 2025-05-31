374cad8f8ef6635187fd7961b3484b1a95225d16
KatrinDer <katrin.lehe@gmail.com>
2025-05-31 19:39:31 +0300

    bus_times

---
+++ bus_times.R
@@ -0,0 +1,5 @@
+# Compiling a table with bus times
+Zoo <- c("08:05", "08:16", "08:28", "08:38", "08:48", "08:59") # departure from Zoo stop
+Toompark <- c("08:18", "08:30", "08:41", "08:51", "09:01", "09:21") # arrival at Toompargi stop
+bus_times <- data.frame(Zoo,Toompark)
+head(bus_times)
